5bb5673a7cbd7819f6485b9d03b7d5e702d9a7d5
Ilya <ilya.m.polyakov@gmail.com>
2015-08-05 19:35:26 -0400

    Set up gruntfile.js with sass tasks. End of 4.8

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,30 @@ module.exports = function(grunt){
     pkg: grunt.file.readJSON('package.json'),
 
     /**
+    * Sass task
+    */
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          sourcemap: 'none',
+        }, // options
+        files: {
+          'compiled/style.css': 'sass/style.scss'
+        } // files
+      }, // dev
+      dist: {
+        options: {
+          style: 'compressed',
+          sourcemap: 'none',
+        }, // options
+        files: {
+          'compiled/style-min.css': 'sass/style.scss'
+        } // files
+      } // dist
+    }, // Sass task
+
+    /**
     *Watch task
     */
     watch: {
@@ -16,6 +40,7 @@ module.exports = function(grunt){
 
   }); // grunt.initConfig
 
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default',['watch']);
 
