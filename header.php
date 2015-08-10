a9c3ec75ba61490b90770c6a6c15c7ccdaf97be9
Ilya <ilya.m.polyakov@gmail.com>
2015-08-10 17:48:32 -0400

    Styling nav. Colors not set up properly

--- header.php
+++ header.php
@@ -26,12 +26,14 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+                                            <div class="title-box">
 			<?php if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php endif; ?>
 			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+                                            </div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
