fd555bec38ae6675d64ed8fe7bed12f3577603fc
Ilya <ilya.m.polyakov@gmail.com>
2015-08-11 15:38:45 -0400

    Changed name of menu from 'primary menu' to 'menu'

--- header.php
+++ header.php
@@ -37,9 +37,12 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'underscoresass' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'underscoresass' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
+
+            
+            
