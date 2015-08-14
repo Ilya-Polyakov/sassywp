87da9fa4098b0181b0bc791ad60b3de1912b7522
Ilya <ilya.m.polyakov@gmail.com>
2015-08-14 15:26:10 -0400

    [content-single.php] Added category to single posts.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,6 +11,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+
+                                    <?php
+                                        /* translators: used between list items, there is a space after the comma */
+                                        $category_list = get_the_category_list( __( ', ', 'underscoresass' ) );
+
+                                        if ( underscoresass_categorized_blog() ) {
+                                            echo '<div class="category-list">' . $category_list . '</div>';
+                                        }
+                                    ?>
+                       
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 		<div class="entry-meta">
@@ -29,6 +39,11 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+            
+                        <?php
+                            echo get_the_tag_list( '<ul><li><i class="fa fa-tag"></i>', '</li><li><i class="fa fa-tag"></i>', '</li></ul>' );
+                        ?>
+            
 		<?php underscoresass_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
