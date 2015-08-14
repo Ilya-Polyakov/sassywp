b7359f63ca17e4a0b579f06476002e19856822fc
Ilya <ilya.m.polyakov@gmail.com>
2015-08-14 09:34:22 -0400

    Added widgets to footer. Unstyled.

--- footer.php
+++ footer.php
@@ -14,7 +14,8 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+	    <?php get_sidebar( 'footer' ); ?>
+                                    <div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscoresass' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'underscoresass' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscoresass' ), 'underscoresass', '<a href="http://www.impolyakov.com" rel="designer">Ilya Polyakov</a>' ); ?>
--- functions.php
+++ functions.php
@@ -108,6 +108,17 @@ function underscoresass_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+        
+        	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widgets', 'underscoresass' ),
+                                    'description' => __('Footer widget area appears in the footer of the site', 'underscoresass'),
+		'id'            => 'sidebar-2',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 }
 add_action( 'widgets_init', 'underscoresass_widgets_init' );
 
